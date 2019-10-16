007c438a096ae8eeecc6b662845cd7d737475a01
Alex Roth <jonesroth7@gmail.com>
2019-10-16 17:09:19 -0600

    Add dynamic getAllUserAverageForDay method/testing

--- test/activity-test.js
+++ test/activity-test.js
@@ -1,10 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
-​
 const Activity = require('../src/Activity');
 const UserRepo = require('../src/User-repo');
 const User = require('../src/User');
-​
+
 describe('Activity', function() {
   let activityData;
   let user1;
@@ -14,9 +13,10 @@ describe('Activity', function() {
   let users;
   let userRepo;
   let activity;
+
   beforeEach(function() {
     activityData = [
-      {
+    {
       "userID": 1,
       "date": "2019/06/15",
       "numSteps": 3577,
@@ -152,10 +152,11 @@ describe('Activity', function() {
     {
       "userID": 1,
       "date": "2019/06/23",
-      "numSteps": 9303,
+      "numSteps": 9000,
       "minutesActive": 8,
       "flightsOfStairs": 9
     }];
+
     user1 = new User({id: 1,
       name: "Alex Roth",
       address: "1234 Turing Street, Denver CO 80301-1697",
@@ -164,6 +165,7 @@ describe('Activity', function() {
       dailyStepGoal: 5000,
       friends: [2, 3, 4]
     });
+
     user2 = new User({id: 2,
       name: "Allie McCarthy",
       address: "1235 Turing Street, Denver CO 80301-1697",
@@ -172,6 +174,7 @@ describe('Activity', function() {
       dailyStepGoal: 9000,
       friends: [1, 3, 4]
     });
+
     user3 = new User({id: 3,
       name: "Jerry Seinfield",
       address: "32 Baker Street, Denver CO 12345",
@@ -180,6 +183,7 @@ describe('Activity', function() {
       dailyStepGoal: 20000,
       friends: [1, 2, 4]
     });
+
     user4 = new User({id: 4,
       name: "Patrick the Starfish",
       address: "A rock in the ocean",
@@ -192,6 +196,7 @@ describe('Activity', function() {
     userRepo = new UserRepo(users);
     activity = new Activity(activityData);
   });
+
   it('should take in data', function() {
     expect(activity.activityData[0].userID).to.eql(1);
     expect(activity.activityData[4].date).to.eql("2019/06/15");
@@ -199,18 +204,23 @@ describe('Activity', function() {
     expect(activity.activityData[8].minutesActive).to.eql(41);
     expect(activity.activityData[10].flightsOfStairs).to.eql(24);
   });
+
   it('should return the miles a given user has walked on a given date', function() {
     expect(activity.getMilesFromStepsByDate(1, "2019/06/15", userRepo.users[0])).to.eql(2.9);
   });
+
   it('should return the number of minutes a given user was active for on a given day', function() {
     expect(activity.getActiveMinutesByDate(1, "2019/06/16")).to.eql(12);
   });
+
   it('should return average active minutes in a given week', function() {
     expect(activity.calculateActiveAverageForWeek(1, "2019/06/21", userRepo)).to.eql(40.4);
   });
+
   it('should return true/false if the given user met their step goal on a given day', function() {
     expect(activity.accomplishStepGoal(4, "2019/06/15", userRepo.users[3])).to.eql(false);
   });
+
   it('should return all days that a given user exceeded their step goal', function() {
     expect(activity.getDaysGoalExceeded(1, userRepo.users[0])).to.eql([
       "2019/06/17",
@@ -220,11 +230,68 @@ describe('Activity', function() {
       "2019/06/22",
       "2019/06/23"]);
   });
+
   it('should return the highest number of stairs climbed in a day for all time', function() {
     expect(activity.getStairRecord(11)).to.eql(33);
   });
+
+  it('should return the average flight of stairs for all users on given day', function() {
+    expect(activity.getAllUserAverageForDay("2019/06/15", userRepo, "flightsOfStairs")).to.eql(21.2)
+  })
+
+  it('should return average steps taken for given date for all users', function() {
+    activityData = activityData.push(
+    {
+      "userID": 1,
+      "date": "2019/06/23",
+      "numSteps": 12000,
+      "minutesActive": 13,
+      "flightsOfStairs": 26
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/23",
+      "numSteps": 9000,
+      "minutesActive": 21,
+      "flightsOfStairs": 14
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/23",
+      "numSteps": 2000,
+      "minutesActive": 8,
+      "flightsOfStairs": 9
+    });
+    expect(activity.getAllUserAverageForDay("2019/06/23", userRepo, "numSteps")).to.eql(8000)
+  });
+
+  it('should return average minutes active given date for all users', function() {
+    activityData = activityData.push(
+    {
+      "userID": 1,
+      "date": "2019/06/23",
+      "numSteps": 12000,
+      "minutesActive": 13,
+      "flightsOfStairs": 26
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/23",
+      "numSteps": 9000,
+      "minutesActive": 21,
+      "flightsOfStairs": 14
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/23",
+      "numSteps": 2000,
+      "minutesActive": 8,
+      "flightsOfStairs": 9
+    });
+    expect(activity.getAllUserAverageForDay("2019/06/23", userRepo, "minutesActive")).to.eql(12.5)
+  });
 })
-​
+
 describe('Friend Activity', function(){
   let activityData;
   let activity;
@@ -235,158 +302,158 @@ describe('Friend Activity', function(){
   let user5;
   let users;
   let userRepo;
-​
+
   beforeEach(function() {
     activityData = [
-      {
-        "userID": 1,
-        "date": "2019/06/15",
-        "numSteps": 3577,
-        "minutesActive": 140,
-        "flightsOfStairs": 16
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/14",
-        "numSteps": 4294,
-        "minutesActive": 138,
-        "flightsOfStairs": 10
-      },
-      {
-        "userID": 3,
-        "date": "2019/06/13",
-        "numSteps": 7402,
-        "minutesActive": 116,
-        "flightsOfStairs": 33
-      },
-      {
-        "userID": 4,
-        "date": "2019/06/12",
-        "numSteps": 3486,
-        "minutesActive": 114,
-        "flightsOfStairs": 32
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/14",
-        "numSteps": 11374,
-        "minutesActive": 213,
-        "flightsOfStairs": 13
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/13",
-        "numSteps": 14810,
-        "minutesActive": 287,
-        "flightsOfStairs": 18
-      },
-      {
-        "userID": 3,
-        "date": "2019/06/12",
-        "numSteps": 2634,
-        "minutesActive": 107,
-        "flightsOfStairs": 5
-      },
-      {
-        "userID": 4,
-        "date": "2019/06/11",
-        "numSteps": 10333,
-        "minutesActive": 114,
-        "flightsOfStairs": 31
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/02",
-        "numSteps": 6389,
-        "minutesActive": 41,
-        "flightsOfStairs": 33
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/03",
-        "numSteps": 8015,
-        "minutesActive": 106,
-        "flightsOfStairs": 37
-      },
-      {
-        "userID": 3,
-        "date": "2019/06/19",
-        "numSteps": 11652,
-        "minutesActive": 20,
-        "flightsOfStairs": 24
-      },
-      {
-        "userID": 4,
-        "date": "2019/06/15",
-        "numSteps": 9256,
-        "minutesActive": 108,
-        "flightsOfStairs": 2
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/16",
-        "numSteps": 3578,
-        "minutesActive": 140,
-        "flightsOfStairs": 16
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/17",
-        "numSteps": 3579,
-        "minutesActive": 140,
-        "flightsOfStairs": 16
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/18",
-        "numSteps": 3580,
-        "minutesActive": 140,
-        "flightsOfStairs": 16
-      }];
-​
-      activity = new Activity(activityData);
-      user1 = new User({id: 1,
-        name: "Alex Roth",
-        address: "1234 Turing Street, Denver CO 80301-1697",
-        email: "alex.roth1@hotmail.com",
-        strideLength: 4.3,
-        dailyStepGoal: 10000,
-        friends: [2, 3, 4]
-      });
-      user2 = new User({id: 2,
-        name: "Allie McCarthy",
-        address: "1235 Turing Street, Denver CO 80301-1697",
-        email: "allie.mcc1@hotmail.com",
-        strideLength: 3.3,
-        dailyStepGoal: 9000,
-        friends: [1, 3, 4]
-      });
-      user3 = new User({
-        id: 3,
-        name: "The Rock",
-        address: "1236 Awesome Street, Denver CO 80301-1697",
-        email: "therock@hotmail.com",
-        strideLength: 10,
-        dailyStepGoal: 60000,
-        friends: [1, 2, 4]
-      });
-​
-      user4 = new User({
-        id: 4,
-        name: "Rainbow Dash",
-        address: "1237 Equestria Street, Denver CO 80301-1697",
-        email: "rainbowD1@hotmail.com",
-        strideLength: 3.8,
-        dailyStepGoal: 7000,
-        friends: [1, 2]
-      });
-​
-​
-      users = [user1, user2, user3, user4];
-      userRepo = new UserRepo(users);
-​
+    {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/14",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/13",
+      "numSteps": 7402,
+      "minutesActive": 116,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/12",
+      "numSteps": 3486,
+      "minutesActive": 114,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/14",
+      "numSteps": 11374,
+      "minutesActive": 213,
+      "flightsOfStairs": 13
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/13",
+      "numSteps": 14810,
+      "minutesActive": 287,
+      "flightsOfStairs": 18
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/12",
+      "numSteps": 2634,
+      "minutesActive": 107,
+      "flightsOfStairs": 5
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/11",
+      "numSteps": 10333,
+      "minutesActive": 114,
+      "flightsOfStairs": 31
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/02",
+      "numSteps": 6389,
+      "minutesActive": 41,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/03",
+      "numSteps": 8015,
+      "minutesActive": 106,
+      "flightsOfStairs": 37
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/19",
+      "numSteps": 11652,
+      "minutesActive": 20,
+      "flightsOfStairs": 24
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/15",
+      "numSteps": 9256,
+      "minutesActive": 108,
+      "flightsOfStairs": 2
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numSteps": 3578,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numSteps": 3579,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/18",
+      "numSteps": 3580,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    }];
+
+    activity = new Activity(activityData);
+
+    user1 = new User({id: 1,
+      name: "Alex Roth",
+      address: "1234 Turing Street, Denver CO 80301-1697",
+      email: "alex.roth1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [2, 3, 4]
+    });
+
+    user2 = new User({id: 2,
+      name: "Allie McCarthy",
+      address: "1235 Turing Street, Denver CO 80301-1697",
+      email: "allie.mcc1@hotmail.com",
+      strideLength: 3.3,
+      dailyStepGoal: 9000,
+      friends: [1, 3, 4]
+    });
+
+    user3 = new User({
+      id: 3,
+      name: "The Rock",
+      address: "1236 Awesome Street, Denver CO 80301-1697",
+      email: "therock@hotmail.com",
+      strideLength: 10,
+      dailyStepGoal: 60000,
+      friends: [1, 2, 4]
+    });
+
+    user4 = new User({
+      id: 4,
+      name: "Rainbow Dash",
+      address: "1237 Equestria Street, Denver CO 80301-1697",
+      email: "rainbowD1@hotmail.com",
+      strideLength: 3.8,
+      dailyStepGoal: 7000,
+      friends: [1, 2]
+    });
+    users = [user1, user2, user3, user4];
+    userRepo = new UserRepo(users);
   });
-​
+
   it('should get a users friend lists activity', function() {
     expect(activity.getFriendsActivity(user4, userRepo)).to.eql([{
       "userID": 1,
@@ -452,6 +519,7 @@ describe('Friend Activity', function(){
       "flightsOfStairs": 37
     }]);
   });
+
   it('should get a users ranked friendslist activity for a chosen week', function() {
     expect(activity.getFriendsAverageStepsForWeek(user4, "2019/06/15", userRepo)).to.eql([{
       '2': 9552
@@ -460,11 +528,12 @@ describe('Friend Activity', function(){
       '1': 7475.5
     }]);
   });
+
   it('should get a users ranked friendslist activity for a chosen week with names', function() {
     expect(activity.showChallengeListAndWinner(user4, "2019/06/15", userRepo)).to.eql([
       'Allie McCarthy: 9552', 'Alex Roth: 7475.5'])
-​
   });
+
   it('should show a 3-day increasing streak for a users step count', function() {
     expect(activity.getStepStreak(userRepo, 1)).to.eql(['2019/06/17', '2019/06/18'])
   });
